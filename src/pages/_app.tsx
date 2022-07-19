f9113975842ae6ed8e2305f851d038f44c575171
Yousaf Wazir <107571370+UmbrellaCrow612@users.noreply.github.com>
2022-07-19 09:29:30 +0100

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { SessionProvider } from 'next-auth/react'
+import '../styles/globals.css'
+
+import { ThemeProvider as ComponentProvider } from '@material-tailwind/react'
+import { GlobalLayout } from '../layouts'
+import type { AppProps } from 'next/app'
+
+// Use of the <SessionProvider> is mandatory to allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <SessionProvider session={pageProps.session} refetchInterval={0}>
+      <ComponentProvider>
+        <GlobalLayout>
+          <Component {...pageProps} />
+        </GlobalLayout>
+      </ComponentProvider>
+    </SessionProvider>
+  )
+}
